6165f463d36f045c889c1fccaef9272b1ea2a8d5
CyberMobius <brimyth@gmail.com>
2015-01-29 19:25:09 -0500

    1/29/15
    Added rectangle detection

--- src/org/usfirst/frc/team321/robot/subsystems/Camera.java
+++ src/org/usfirst/frc/team321/robot/subsystems/Camera.java
@@ -1,43 +1,27 @@
 package org.usfirst.frc.team321.robot.subsystems;
 
 import com.ni.vision.NIVision;
-import com.ni.vision.NIVision.ColorMode;
 import com.ni.vision.NIVision.Image;
-import com.ni.vision.NIVision.Range;
-
-
+import com.ni.vision.NIVision.ROI;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
 
 /**
  *
  */
 public class Camera extends Subsystem {
 	
-	int session;
-    Image frame;
-    Image frameBin;
+	Image frame;
     CameraServer camServer = CameraServer.getInstance();
-    SendableChooser putter;
-    Range rangeIR;
-
+    ROI roi;
+    
     public Camera(){	
         frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
     	
         camServer.setQuality(50); //TODO: Configure camera quality
-        camServer.startAutomaticCapture("cam0");
-        
-//        frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_HSL, 0);//Creates a blank HSL image
-//        frameBin = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_U8,0);//Creates blank binary image
-//        
-//        session = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-//        
-//        
-//        rangeIR = new Range(260,300);//Sets hue range for IR on the camera
-//        
-//        NIVision.IMAQdxConfigureGrab(session); 
+        camServer.startAutomaticCapture("cam0");    
+                
     }
     
     public void initDefaultCommand() {
@@ -45,26 +29,15 @@ public class Camera extends Subsystem {
     
     
     public void test(){
-    	 NIVision.IMAQdxStartAcquisition(session);
 
+    	 NIVision.imaqDetectRectangles(frame, 
+    			 new NIVision.RectangleDescriptor(/*0,0,50,50*/),
+    			 new NIVision.CurveOptions(/*NIVision.ExtractionMode.NORMAL_IMAGE, 5 ,NIVision.EdgeFilterSize.NORMAL, 20,20,20,20,20,20*/), 
+    			 new NIVision.ShapeDetectionOptions(),
+    			 roi //I think it takes in this blank object and writes onto it
+    	 );
        
 
-         //while (processing) {
-
-             NIVision.IMAQdxGrab(session, frame, 1);//finally links the blank image to the camera feed, and I think the 1 just tells it to wait to be ready to grab another image??????
-             
-             CameraServer.getInstance().setImage(frame);//outputs image instead of outputting directly from the camera 
-             
-             //So once I'm here, I have the image with which to process
-             
-             NIVision.imaqColorThreshold(frameBin, frame, session, ColorMode.HSL, rangeIR, null, null);//Takes in frame and writes on frameBin if a certain pixel's hue is within the IR range
-             //From here I'll work with the binary image
-             
-             CameraServer.getInstance().setImage(frameBin);
-             
-             //Timer.delay(0.005);
-         //}
-         NIVision.IMAQdxStopAcquisition(session);//When you're done processing this will tell the camera it's not working anymore
     }
 }
 
